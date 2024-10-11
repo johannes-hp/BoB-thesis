65ad829e8305bd24f189c6728d06d93dbe54153c
johannes-hp <johannes@hotmail.com>
2024-10-11 11:08:41 +0200

    Add annotation files

--- epitome-scripts/epitome_annotation.py
+++ epitome-scripts/epitome_annotation.py
@@ -1,6 +1,22 @@
 import cltk
 import os
 import pandas as pd
+import numpy as np
+
+def annotation_dataframe(text: str, cltk_nlp) -> pd.DataFrame:
+    """
+    Takes a text string and a cltk.NLP instance and creates a 
+    DataFrame with tokens, POS-tags and an empty column for 
+    annotations.
+    """
+
+    text_doc = cltk_nlp.analyze(text)
+    text_df = pd.DataFrame()
+    text_df['tokens'] = text_doc.tokens
+    text_df['pos'] = text_doc.pos
+    text_df['annotation'] = ['' for _ in range(len(text_doc.tokens))]
+
+    return text_df
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,31 +26,12 @@ lat_cltk.pipeline.processes.remove(cltk.lexicon.processes.LatinLexiconProcess)
 lat_cltk.pipeline.processes.remove(cltk.embeddings.processes.LatinEmbeddingsProcess)
 # remove process since it slows the code (it generates word2vec embeddings)
 
-with open(os.path.join(cwd, '../epitomes/e0190.txt'), 'r', encoding='utf-8') as f:
-     text190 = f.readlines()[-1].strip()
+epitome_numbers = [190, 286, 432, 584, 915, 1406, 1616, 1770, 1904, 2309]
 
-with open(os.path.join(cwd, '../epitomes/e0432.txt'), 'r', encoding='utf-8') as f:
-    text432 = f.readlines()[-1].strip()
+epitome_numbers = [f'e{n:04}' for n in epitome_numbers]
 
-with open(os.path.join(cwd, '../epitomes/e1904.txt'), 'r', encoding='utf-8') as f:
-    text1904 = f.readlines()[-1].strip()
-
-text190_doc = lat_cltk.analyze(text190)
-text432_doc = lat_cltk.analyze(text432)
-text1904_doc = lat_cltk.analyze(text1904)
-
-text190_df = pd.DataFrame()
-text190_df['tokens'] = text190_doc.tokens
-text190_df['pos'] = text190_doc.pos
-
-text432_df = pd.DataFrame()
-text432_df['tokens'] = text432_doc.tokens
-text432_df['pos'] = text432_doc.pos
-
-text1904_df = pd.DataFrame()
-text1904_df['tokens'] = text1904_doc.tokens
-text1904_df['pos'] = text1904_doc.pos
-
-text190_df.to_csv(os.path.join(cwd, '../annotation/e0190.csv'))
-text432_df.to_csv(os.path.join(cwd, '../annotation/e0432.csv'))
-text1904_df.to_csv(os.path.join(cwd, '../annotation/e1904.csv'))
+for epitome in epitome_numbers:
+    with open(os.path.join(cwd, f'../epitomes/{epitome}.txt'), 'r', encoding='utf-8') as f:
+        text = f.readlines()[-1].strip()
+        text_df = annotation_dataframe(text, lat_cltk)
+        text_df.to_csv(os.path.join(cwd, f'../annotation/{epitome}.csv'))
